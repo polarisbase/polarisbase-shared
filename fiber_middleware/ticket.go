ca7653cee4ca7ed54cb1628389667b0c67c848e1
nomadhills <75098873+nomadhills@users.noreply.github.com>
2023-10-18 16:50:04 -0400

    up

--- fiber_middleware/ticket.go
+++ fiber_middleware/ticket.go
@@ -2,7 +2,7 @@ package fiber_middleware
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/polarisbase/polaris-sdk/v3/shared"
+	"github.com/polarisbase/polarisbase-shared"
 )
 
 func Tickets(
